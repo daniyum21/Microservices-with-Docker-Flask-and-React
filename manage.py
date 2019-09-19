73933587f4185438b1e40746a559d7826682e9b9
Daniel Nizeyumukiza <daniyum21@gmail.com>
2019-09-19 00:36:04 -0700

    =Added tests

--- manage.py
+++ manage.py
@@ -2,11 +2,30 @@
 
 
 from flask.cli import FlaskGroup
+import sys
+import unittest
 
-from project import app
+from project import app, db  # new
 
 
 cli = FlaskGroup(app)
+
+
+# new
+@cli.command('recreate_db')
+def recreate_db():
+    db.drop_all()
+    db.create_all()
+    db.session.commit()
+
+@cli.command()
+def test():
+    """Runs the tests without code coverage"""
+    tests = unittest.TestLoader().discover('project/tests', pattern='test*.py')
+    result = unittest.TextTestRunner(verbosity=2).run(tests)
+    if result.wasSuccessful():
+        return 0
+    sys.exit(result)
 
 
 if __name__ == '__main__':
